a9b65b29c8ba0219ca4e16b196fc18bb862179ef
Luong Nguyen Intern <luong.nh@codecomplete.jp>
2022-09-30 20:49:40 +0700

    done role

--- app/screens/DoctorUpdateProfile-screen.tsx
+++ app/screens/DoctorUpdateProfile-screen.tsx
@@ -1,23 +1,48 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../navigators"
-// import { firebase } from "@react-native-firebase/database"
+import { MyHeader } from "../components/MyHeader"
+import { firebase } from "@react-native-firebase/database"
+import { Button } from "@rneui/themed"
 
 export const DoctorUpdateProfileScreen: FC<
   StackScreenProps<NavigatorParamList, "doctorUpdateProfile">
-> = observer(function DoctorUpdateProfileScreen() {
-  // firebase
-  //   .app()
-  //   .database(
-  //     "https://healthcare-856bd-default-rtdb.asia-southeast1.firebasedatabase.app",
-  //   )
-  //   .ref("/users/" + uid)
-  //   .set({
-  //     yearExp: 5,
-  //     type: "clgt",
-  //   })
-  //   .then(() => console.log("Data set."))
-  return <View></View>
+> = observer(function DoctorUpdateProfileScreen({ navigation }) {
+  const user = firebase.auth().currentUser
+
+  const updateInfoDoctor = () => {
+    firebase
+      .app()
+      .database("https://healthcare-856bd-default-rtdb.asia-southeast1.firebasedatabase.app")
+      .ref("/doctors/" + user.uid)
+      .set({
+        name: user.displayName,
+        email: user.email,
+        phoneNumber: "0383334687",
+        dayStartWork: "20/10/2005",
+        department: "Internal medicine doctor",
+        age: 48,
+        gender: true, //true is male - false is female
+      })
+      .then(() => console.log("Update Info Successfully !!"))
+  }
+
+  return (
+    <View style={styles.container}>
+      <MyHeader title="Doctor Profile" onPress={() => navigation.goBack()} />
+      <Button title={"Save Info Doctor"} onPress={updateInfoDoctor} />
+    </View>
+  )
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleHeader: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
 })
--- app/screens/UserUpdateProfile-screen.tsx
+++ app/screens/UserUpdateProfile-screen.tsx
@@ -1,11 +1,50 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../navigators"
+import { MyHeader } from "../components/MyHeader"
+import { Button } from "@rneui/themed"
+import { firebase } from "@react-native-firebase/database"
 
 export const UserUpdateProfileScreen: FC<
   StackScreenProps<NavigatorParamList, "userUpdateProfile">
-> = observer(function UserUpdateProfileScreen() {
-  return <View></View>
+> = observer(function UserUpdateProfileScreen({ navigation }) {
+  const user = firebase.auth().currentUser
+
+  const updateInfoUser = () => {
+    firebase
+      .app()
+      .database("https://healthcare-856bd-default-rtdb.asia-southeast1.firebasedatabase.app")
+      .ref("/users/" + user.uid)
+      .set({
+        name: user.displayName,
+        email: user.email,
+        phoneNumber: "0368440510",
+        birthday: "05/10/2002",
+        age: 48,
+        gender: true, //true is male - false is female
+        height: 175,
+        weight: 60,
+        heartbeat: 75,
+        bloodPressure: "120/80",
+      })
+      .then(() => console.log("Update Info Successfully !!"))
+  }
+  return (
+    <View style={styles.container}>
+      <MyHeader title="User Profile" onPress={() => navigation.goBack()} />
+      <Button title={"Save Info User"} onPress={updateInfoUser} />
+    </View>
+  )
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleHeader: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
 })
