9e0d89a50ecef21d075ebb5ddb9138590812de9a
LuongNH <protate3008@gmail.com>
2022-10-14 00:23:56 +0700

    done update profile

--- app/screens/UserUpdateProfile-screen.tsx
+++ app/screens/UserUpdateProfile-screen.tsx
@@ -1,59 +1,85 @@
-import React, { FC, useState } from "react"
+import React, { FC, useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ScrollView, StyleSheet, TextInput, TouchableOpacity, View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../navigators"
 import { MyHeader } from "../components/MyHeader"
-import { Button, Text } from "@rneui/themed"
+import { Input, Text } from "@rneui/themed"
 import { firebase } from "@react-native-firebase/database"
 import { database } from "../../configs/firebase"
 import { color } from "../theme"
 import { CustomText } from "../components/CustomText"
-import { moderateScale, scale, verticleScale } from "../utils/Scale/Scaling"
-import DatePicker from "react-native-date-picker"
-import moment from "moment"
-import { format } from "date-fns"
+import { scale, verticleScale } from "../utils/Scale/Scaling"
 import RadioForm from "react-native-simple-radio-button"
 import { CustomButton } from "../components/CustomButton"
 import Slider from "@react-native-community/slider"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+import DatePicker from "react-native-date-picker"
+import moment from "moment"
 
 export const UserUpdateProfileScreen: FC<
   StackScreenProps<NavigatorParamList, "userUpdateProfile">
 > = observer(function UserUpdateProfileScreen({ navigation }) {
-  const user = firebase.auth().currentUser
-  const [email, setEmail] = useState(user.email)
-  const [weight, setWeight] = useState(50)
+  const [infoUser, setInfoUser] = useState<InfoUser>()
+  const [minBP, setMinBP] = useState("")
+  const [maxBP, setMaxBP] = useState("")
+  const [email, setEmail] = useState("")
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
   const [heartbeat, setHeartbeat] = useState("")
   const [phone, setPhone] = useState("")
-  const [height, setHeight] = useState(100)
-  const [gender, setGender] = useState(true)
-
+  const [gender, setGender] = useState()
+  const [birthday, setBirthDay] = useState(new Date())
+  const [open, setOpen] = useState(false)
+  const [date, setDate] = useState("")
   const options = [
     { label: "Nam", value: true },
     { label: "Nữ", value: false },
   ]
+  useEffect(() => {
+    database.ref("/users/" + firebase.auth().currentUser.uid).on("value", (snapshot) => {
+      const [min, max] = snapshot.val().bloodPressure.split("/")
+      setInfoUser(snapshot.val())
+      setGender(snapshot.val().gender)
+      setMinBP(min)
+      setMaxBP(max)
+      setEmail(snapshot.val().email)
+      setHeartbeat(snapshot.val().heartbeat)
+      setHeight(snapshot.val().height)
+      setWeight(snapshot.val().weight)
+      setPhone(snapshot.val().phoneNumber)
+      setDate(snapshot.val().birthday)
+    })
+    return () => {
+      setInfoUser(null)
+      setGender(undefined)
+      setMinBP(undefined)
+      setMaxBP(undefined)
+      setEmail(undefined)
+      setHeartbeat(undefined)
+      setHeight(undefined)
+      setWeight(undefined)
+      setPhone(undefined)
+      setDate(undefined)
+    }
+  }, [])
 
-  const [birtday, setBirtday] = useState(new Date())
-  const formattedDate = format(birtday, "dd/MM/yyyy")
-
-  const [open, setOpen] = useState(false)
-
-  const updateInfoUser = () => {
+  const updateInfoUser = (email, phone, date, gender, height, weight, heartbeat, minBP, maxBP) => {
     database
-      .ref("/users/" + user.uid)
+      .ref("/users/" + firebase.auth().currentUser.uid)
       .set({
-        uid: user.uid,
-        name: user.displayName,
+        uid: firebase.auth().currentUser.uid,
+        name: firebase.auth().currentUser.displayName,
         email: email,
         photoUrl: "https://i.pinimg.com/originals/12/61/dd/1261dda75d943cbd543cb86c15f31baa.jpg",
         phoneNumber: phone,
-        birthday: formattedDate,
-        age: 48,
+        birthday: date,
         gender: gender, //true is male - false is female
         height: height,
         weight: weight,
-        heartbeat: 75,
-        bloodPressure: "120/80",
+        heartbeat: heartbeat,
+        bloodPressure: `${minBP}/${maxBP}`,
       })
       .then(() => {
         console.log("Update Info Successfully !!")
@@ -63,17 +89,53 @@ export const UserUpdateProfileScreen: FC<
   return (
     <View style={styles.container}>
       <MyHeader title="User Profile" onPress={() => navigation.goBack()} />
-      {/* <ScrollView> */}
       <View style={styles.content}>
-        <TextInput style={styles.input} placeholder="Phone" onChangeText={setPhone} />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          onChangeText={setEmail}
-          defaultValue={user.email}
+        <Input
+          containerStyle={styles.input}
+          placeholder="Phone"
+          onChangeText={(e) => setPhone(e)}
+          value={phone}
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          leftIcon={<Ionicons name="call" size={24} color="gray" />}
         />
+        <Input
+          containerStyle={styles.input}
+          placeholder="Email"
+          onChangeText={(e) => setEmail(e)}
+          value={email}
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          leftIcon={<Ionicons name="mail" size={24} color="gray" />}
+        />
+        <Input
+          containerStyle={styles.input}
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          placeholder="Birth Day DD/MM/YYYY"
+          value={date}
+          onChangeText={(e) => setDate(e)}
+          leftIcon={
+            <Ionicons name="calendar" size={24} color="gray" onPress={() => setOpen(true)} />
+          }
+        />
+        <DatePicker
+          title="Select Day"
+          mode="date"
+          modal
+          open={open}
+          date={birthday}
+          onConfirm={(date) => {
+            setOpen(false)
+            setBirthDay(date)
+            setDate(moment(date).format("DD/MM/yyyy"))
+          }}
+          onCancel={() => {
+            setOpen(false)
+          }}
+        />
+        <Text style={{ color: "#000", fontSize: 20, fontWeight: "bold", margin: 8 }}>
+          Info Health
+        </Text>
         <View style={styles.boxSlider}>
-          <CustomText title={height + "cm"}></CustomText>
+          <CustomText title={"Height: " + height + " cm"}></CustomText>
           <Slider
             style={styles.height}
             maximumValue={250}
@@ -81,40 +143,48 @@ export const UserUpdateProfileScreen: FC<
             minimumTrackTintColor="#cccc"
             maximumTrackTintColor="#cccc"
             step={1}
-            value={height}
+            value={infoUser?.height}
             onValueChange={(height) => setHeight(height)}
           />
         </View>
         <View style={styles.boxSlider}>
-          <CustomText title={weight + "kg"}></CustomText>
+          <CustomText title={"Weight: " + weight + " kg"}></CustomText>
           <Slider
             style={styles.height}
-            minimumValue={50}
-            maximumValue={250}
+            minimumValue={20}
+            maximumValue={200}
             minimumTrackTintColor="#cccc"
             maximumTrackTintColor="#cccc"
             step={1}
-            value={weight}
+            value={infoUser?.weight}
             onValueChange={(weight) => setWeight(weight)}
           />
         </View>
-        <View style={styles.boxBirtday}>
-          <TouchableOpacity style={styles.btnBirtday} onPress={() => setOpen(true)}>
-            {/* <CustomText title={"Birt Day : " + formattedDate}></CustomText> */}
-            <Text style={styles.textBirtDay}>{"Birt Day : " + formattedDate}</Text>
-          </TouchableOpacity>
-          <DatePicker
-            modal
-            mode="date"
-            open={open}
-            date={birtday}
-            onConfirm={(birtday) => {
-              setOpen(false)
-              setBirtday(birtday)
-            }}
-            onCancel={() => {
-              setOpen(false)
-            }}
+        <Input
+          containerStyle={styles.input}
+          placeholder="Heartbeat"
+          onChangeText={(e) => setHeartbeat(e)}
+          defaultValue={heartbeat}
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          leftIcon={<FontAwesome name="heartbeat" size={24} color="gray" />}
+        />
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={{ color: "#000", fontSize: 16, fontWeight: "bold", margin: 8 }}>
+            Blood Pressure
+          </Text>
+          <Input
+            containerStyle={styles.inputBloodPressure}
+            placeholder="Min"
+            onChangeText={(e) => setMinBP(e)}
+            value={minBP}
+            inputContainerStyle={{ borderBottomWidth: 0 }}
+          />
+          <Input
+            containerStyle={styles.inputBloodPressure}
+            placeholder="Max"
+            onChangeText={(e) => setMaxBP(e)}
+            value={maxBP}
+            inputContainerStyle={{ borderBottomWidth: 0 }}
           />
         </View>
 
@@ -132,10 +202,14 @@ export const UserUpdateProfileScreen: FC<
           />
         </View>
         <View style={styles.boxButton}>
-          <CustomButton title={"Save Info User"} onPress={updateInfoUser} />
+          <CustomButton
+            title={"Save Info User"}
+            onPress={() =>
+              updateInfoUser(email, phone, date, gender, height, weight, heartbeat, minBP, maxBP)
+            }
+          />
         </View>
       </View>
-      {/* </ScrollView> */}
     </View>
   )
 })
@@ -143,7 +217,6 @@ export const UserUpdateProfileScreen: FC<
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f3feff",
   },
   titleHeader: {
     fontSize: 24,
@@ -158,15 +231,18 @@ const styles = StyleSheet.create({
     textAlign: "center",
     height: 50,
     width: 300,
-    // borderWidth: 1,
     margin: 15,
-    // borderColor: color.colorHeader,
     borderRadius: 16,
-    backgroundColor: color.text,
-    shadowColor: "#60c0f0",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
+    backgroundColor: color.background,
+  },
+  inputBloodPressure: {
+    textAlign: "center",
+    height: 50,
+    width: 120,
+    marginVertical: 15,
+    marginHorizontal: 4,
+    borderRadius: 12,
+    backgroundColor: color.background,
   },
   boxSlider: {
     margin: 12,
@@ -175,41 +251,16 @@ const styles = StyleSheet.create({
   height: {
     width: scale(250),
   },
-  boxBirtday: {
-    justifyContent: "center",
-    margin: 12,
-    alignItems: "center",
-    width: scale(250),
-    height: 50,
-    backgroundColor: color.text,
-    borderRadius: 15,
-    shadowColor: "#60c0f0",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  btnBirtday: {
-    width: scale(200),
-    margin: 8,
-  },
   boxGender: {
     marginTop: 20,
     marginBottom: 20,
     marginLeft: scale(30),
-    // flexDirection: "row",
-    // backgroundColor: color.line,
     width: scale(200),
     alignItems: "center",
     flex: 1,
   },
   boxButton: {
-    marginBottom: verticleScale(40),
-  },
-  textBirtDay: {
-    color: color.storybookDarkBg,
-    fontWeight: "600",
-    fontSize: moderateScale(16),
-    textAlign: "center",
+    marginBottom: verticleScale(20),
   },
   radioButton: {
     paddingLeft: 8,
