2b2f7367f1c7de86f5f294bfbdf26c3dab609c30
LuongNH <luongnh0510@gmail.com>
2022-11-02 23:06:00 +0700

    done details booking

--- app/screens/DetailsBooking-screen.tsx
+++ app/screens/DetailsBooking-screen.tsx
@@ -2,11 +2,13 @@ import React, { FC, useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../navigators"
-import { Dimensions, StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { MyHeader } from "../components/MyHeader"
 import { database } from "../../configs/firebase"
 import { Dialog, Image } from "@rneui/themed"
 import { CustomButton } from "../components/CustomButton"
+import AntDesign from "react-native-vector-icons/AntDesign"
+import { scale } from "../utils/Scale/Scaling"
 
 export const DetailsBookingScreen: FC<StackScreenProps<NavigatorParamList, "detailsBooking">> =
   observer(function DetailsBookingScreen({ navigation, route }) {
@@ -64,6 +66,18 @@ export const DetailsBookingScreen: FC<StackScreenProps<NavigatorParamList, "deta
       }
       return "--:--"
     }
+    const getStatus = (s, d) => {
+      if (s == 1 && getAge(d) < 0) {
+        return "Unfinished"
+      }
+      if (s == 2) {
+        return "In the past"
+      }
+      if (s == 3) {
+        return "Canceled"
+      }
+      return "In the past"
+    }
     if (!infoDoctor) {
       return (
         <View style={styles.container}>
@@ -78,46 +92,101 @@ export const DetailsBookingScreen: FC<StackScreenProps<NavigatorParamList, "deta
       <View style={styles.container}>
         <MyHeader title="Details Booking" onPress={() => navigation.goBack()}></MyHeader>
         <View style={styles.content}>
-          <View style={{ flex: 2, backgroundColor: "#000", width: "95%" }}></View>
-          <View style={{ flex: 3, backgroundColor: "#ccc", width: "95%" }}></View>
-          {/* <View style={{ alignItems: "center" }}>
-            <Image style={styles.imgDoctor} source={{ uri: infoDoctor.photoUrl }} />
-            <Text style={styles.textInfo}>{infoDoctor.name}</Text>
-            <View style={{ flexDirection: "row" }}>
-              <Text style={{ color: "#000" }}>{infoDoctor.department}</Text>
-              <Text style={{ color: "#000" }}> - {getAge(infoDoctor.dayStartWork)} YoE+</Text>
+          <View style={styles.cardDoctor}>
+            <View style={styles.infoDoctor}>
+              <Image style={styles.imgDoctor} source={{ uri: infoDoctor.photoUrl }} />
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                  height: 200,
+                }}
+              >
+                <View>
+                  <Text style={[styles.textInfo, { marginTop: 16 }]}>{infoDoctor.name}</Text>
+                  <Text style={{ color: "#000" }}>{infoDoctor.department}</Text>
+                  <Text style={styles.textInfoSmall}>Mail: {infoDoctor.email}</Text>
+                  <Text style={styles.textInfoSmall}>Phone: {infoDoctor.phoneNumber}</Text>
+                </View>
+                <TouchableOpacity>
+                  <View
+                    style={{
+                      height: 66,
+                      width: 66,
+                      backgroundColor: "#f9f9f9",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      borderRadius: 16,
+                      flexDirection: "row",
+                    }}
+                  >
+                    <Text style={{ fontSize: 20, color: "#000", fontWeight: "bold" }}>+1 </Text>
+                    <AntDesign name="heart" size={24} color="red" />
+                  </View>
+                </TouchableOpacity>
+                <View />
+              </View>
+            </View>
+            <View style={styles.infoDoctor}>
+              <Text style={{ color: "#000", fontWeight: "bold", fontSize: 16 }}>
+                {"       " + getAge(infoDoctor.dayStartWork) + "\n Years exp"}
+              </Text>
+              <Text style={{ color: "#000", fontWeight: "bold", fontSize: 16 }}>
+                100 {""} <AntDesign name="heart" size={20} color="#000" />
+              </Text>
             </View>
           </View>
-
-          <View style={{ marginVertical: 30 }}>
-            <View style={styles.boxInfo}>
-              <Text
-                style={[
-                  styles.textInfo,
-                  { alignSelf: "center", color: "#fff", fontStyle: "normal" },
-                ]}
-              >
-                Infomation
-              </Text>
-              <Text style={styles.textInfoBig}>Date: {booking.date}</Text>
-              <Text style={styles.textInfoBig}>
-                Work Shift: {getWorkShift(booking.workingTime)}
+          <View style={styles.infoBooking}>
+            <View style={styles.boxTime}>
+              <Text style={{ fontSize: 32, color: "#333", fontWeight: "bold" }}>
+                <AntDesign name="calendar" size={32} color="#333" /> {" " + booking.date}
               </Text>
             </View>
-            <View style={styles.boxContact}>
-              <Text
-                style={[
-                  styles.textInfo,
-                  { alignSelf: "center", color: "#fff", fontStyle: "normal" },
-                ]}
-              >
-                Contact
+            <View style={styles.boxTime}>
+              <Text style={{ fontSize: 36, color: "#333", fontWeight: "bold" }}>
+                <AntDesign name="clockcircleo" size={36} color="#333" />
+                {" " + getWorkShift(booking.workingTime)}
               </Text>
-              <Text style={styles.textInfoSmall}>Mail: {infoDoctor.email}</Text>
-              <Text style={styles.textInfoSmall}>Phone: {infoDoctor.phoneNumber}</Text>
             </View>
-          </View> */}
-          <CustomButton title={"Cancel"} onPress={() => handleCancel()} />
+            <View style={styles.boxTime}>
+              <Text
+                style={
+                  getStatus(booking.status, booking.date) === "Unfinished"
+                    ? { fontSize: 36, color: "#8fce00", fontWeight: "bold" }
+                    : getStatus(booking.status, booking.date) === "In the past"
+                    ? { fontSize: 36, color: "#888", fontWeight: "bold" }
+                    : { fontSize: 36, color: "red", fontWeight: "bold" }
+                }
+              >
+                <AntDesign
+                  name={
+                    getStatus(booking.status, booking.date) === "Unfinished"
+                      ? "questioncircle"
+                      : "closecircle"
+                  }
+                  size={36}
+                  color={
+                    getStatus(booking.status, booking.date) === "Unfinished"
+                      ? "#8fce00"
+                      : getStatus(booking.status, booking.date) === "In the past"
+                      ? "#888"
+                      : "red"
+                  }
+                />
+                {" " + getStatus(booking.status, booking.date)}
+              </Text>
+            </View>
+          </View>
+          {getStatus(booking.status, booking.date) === "In the past" ? (
+            <CustomButton
+              title={"New Booking"}
+              onPress={() => navigation.navigate("listDoctors")}
+            />
+          ) : getStatus(booking.status, booking.date) === "Unfinished" ? (
+            <CustomButton title={"Cancel"} onPress={() => handleCancel} />
+          ) : (
+            <View />
+          )}
           <View style={{ marginVertical: 8 }} />
         </View>
       </View>
@@ -131,61 +200,55 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "#fffbe1",
   },
-  cardDoctor: {},
-  infoBooking: {},
+  cardDoctor: {
+    flex: 1,
+    width: "95%",
+    backgroundColor: "#1cc3",
+    borderRadius: 16,
+    marginVertical: 8,
+    justifyContent: "space-around",
+  },
+  infoDoctor: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  infoBooking: {
+    flex: 1,
+    width: "95%",
+    marginVertical: 8,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
   imgDoctor: {
     margin: 8,
-    width: 200,
-    height: 200,
+    width: 150,
+    height: 190,
     resizeMode: "contain",
+    borderWidth: 5,
+    borderColor: "#f9f9f9",
+    borderRadius: 16,
   },
   textInfo: {
-    fontSize: 28,
+    fontSize: 20,
     fontWeight: "bold",
     color: "#000",
     fontStyle: "italic",
   },
   textInfoSmall: {
-    color: "#f1f1f1",
+    color: "#000",
     paddingHorizontal: 4,
     fontSize: 16,
+    fontWeight: "bold",
   },
-  textInfoBig: {
-    color: "#f1f1f1",
-    paddingHorizontal: 4,
-    fontSize: 32,
-  },
-  boxInfo: {
+  boxTime: {
+    width: "90%",
+    height: scale(90),
+    backgroundColor: "#1cc3",
+    borderRadius: 18,
     alignItems: "center",
-    margin: 8,
-    paddingVertical: 8,
-    backgroundColor: "#4ea9fd",
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.55,
-    shadowRadius: 4.84,
-    elevation: 8,
-  },
-  boxContact: {
-    margin: 8,
-    alignSelf: "center",
-    width: 190,
-    marginHorizontal: 4,
-    paddingVertical: 8,
-    backgroundColor: "#4ea9fd",
-    borderRadius: 12,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.55,
-    shadowRadius: 4.84,
-    elevation: 8,
+    justifyContent: "center",
   },
 })
